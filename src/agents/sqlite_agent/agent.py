e873ffc2d78c5c2c155630ddb46f16ee187800ea
taingth <taint@technica.ai>
2025-05-05 11:37:06 +0700

    Update documentation and examples with new code samples and assets

--- src/agents/sqlite_agent/agent.py
+++ src/agents/sqlite_agent/agent.py
@@ -1,25 +1,26 @@
+from contextlib import AsyncExitStack
+
 from google.adk.agents.llm_agent import LlmAgent
 from google.adk.models.lite_llm import LiteLlm
 from google.genai import types
-from contextlib import AsyncExitStack
 
-
-from .mcp_client import get_sqlite_alchemy_client, get_brave_search_client
+from .mcp_client import get_brave_search_client, get_sqlite_alchemy_client
 
 
 async def get_sqlite_agent():
     async_exit_stack = AsyncExitStack()
-    
-    sqlite_tools, _ = await get_sqlite_alchemy_client(async_exit_stack=async_exit_stack)
-    
-    brave_search_tools, _ = await get_brave_search_client(async_exit_stack=async_exit_stack)
 
-    model_name = "hosted_vllm/Qwen/QwQ-32B-AWQ"
-    # model_name = "ollama_chat/qwq:latest"
+    sqlite_tools, _ = await get_sqlite_alchemy_client(async_exit_stack=async_exit_stack)
+
+    brave_search_tools, _ = await get_brave_search_client(
+        async_exit_stack=async_exit_stack
+    )
+
+    # model_name = "openai/Qwen/QwQ-32B-AWQ"
+    model_name = "ollama_chat/qwen3:32b"
     agent = LlmAgent(
         model=LiteLlm(
             model=model_name,
-            api_base="http://0.0.0.0:3001/v1",
         ),
         name="sqlite_agent",
         description="An agent to handle SQLite database queries and can search internet information.",
--- src/model_serving/cli_serving.sh
+++ src/model_serving/cli_serving.sh
@@ -5,7 +5,7 @@ if [ "$choice_framework" = "vllm" ]; then
     uv run python -m vllm.entrypoints.openai.api_server --model Qwen/QwQ-32B-AWQ --enable-reasoning --reasoning-parser deepseek_r1 --enable-auto-tool-choice --tool-call-parser hermes --tensor-parallel-size 2 --max-model-len 8000 --gpu-memory-utilization 0.8 --enforce-eager --port 3001
 elif [ "$choice_framework" = "sglang" ]; then
     echo "Starting SGLang framework..."
-    uv run python -m sglang.launch_server --model-path Qwen/QwQ-32B-AWQ --tp 2 --mem-fraction-static 0.8 --enable-torch-compile --tool-call-parser qwen25 --reasoning-parser deepseek-r1 --port 3001 --host 0.0.0.0 --attention-backend flashinfer
+    uv run python -m sglang.launch_server --model-path Qwen/QwQ-32B-AWQ --tp 2 --mem-fraction-static 0.8 --tool-call-parser qwen25 --reasoning-parser deepseek-r1 --port 3001 --host 0.0.0.0 --attention-backend flashinfer
 else
     echo "Usage: $0 [vllm|sglang]"
     echo "  vllm    - Start the vLLM framework"
